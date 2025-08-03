c430560c2f09c5a3a58b05c8ea709a661e921c8a
willame <willamesilvaop@hotmail.com>
2025-08-03 10:22:41 -0300

    Add logging for submitted food data in CreateModal component

--- cardapioF/src/components/create-modal/create-modal.tsx
+++ cardapioF/src/components/create-modal/create-modal.tsx
@@ -44,6 +44,8 @@ export function CreateModal ({ closeModal}: ModalProps) {
             cozinheiroId,
             cozinheiroNome: cozinheiroNome || ""
         };
+
+        console.log('dadoComida enviado:', dadoComida);
     
         mutate(dadoComida, {
             onError: (error) => {
