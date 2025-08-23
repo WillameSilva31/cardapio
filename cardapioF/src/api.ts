e6afdfe4457fce24ba0ef4f36b42cdedb12ee05a
willame <willamesilvaop@hotmail.com>
2025-08-23 19:53:38 -0300

    api.ts baseURL link

--- cardapioF/src/api.ts
+++ cardapioF/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: '',
+    baseURL: 'https://cardapio-fullstack.onrender.com',
     headers: {
         "Acess-Control-Allow-Origin":"*",
         'Content-Type':'application/json',
