02c311319cc934103ae3e7c14ed01d3195b4007f
willame <willamesilvaop@hotmail.com>
2025-08-23 17:22:03 -0300

    update CORS configuration and Docker setup for local development

--- cardapioB/src/main/java/com/example/cardapio/infra/cors/ConfiguracaoCors.java
+++ cardapioB/src/main/java/com/example/cardapio/infra/cors/ConfiguracaoCors.java
@@ -10,7 +10,7 @@ public class ConfiguracaoCors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://cardapio-frontend-r4iy.onrender.com")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- cardapioF/src/api.ts
+++ cardapioF/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://cardapio-api-1ymc.onrender.com',
+    baseURL: 'http://localhost:8080',
     headers: {
         "Acess-Control-Allow-Origin":"*",
         'Content-Type':'application/json',
